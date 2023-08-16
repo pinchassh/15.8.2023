3b077cd8d167201757d098266cf9a18d051874df
pinchassh <psh28497@gmail.com>
2023-08-16 12:49:32 +0300

    end part B

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const { log } = require('console');
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const uuid = uuidv4()
+// console.log(uuid);
 const app = express();
 const port = 3000;
 const jsonParser = express.json();
 
 const users = [
-    { id: 1, email: 'avi@example.com', password: 'password123' },
-    { id: 2, email: 'dana@example.com', password: 'securepass' },
-    { id: 3, email: 'yael@example.com', password: 'mysecretpw' }
+    { id: uuidv4(), email: 'avi@example.com', password: 'password123' },
+    { id: uuidv4(), email: 'dana@example.com', password: 'securepass' },
+    { id: uuidv4(), email: 'yael@example.com', password: 'mysecretpw' }
 ];
 
-function maxid(users) {
-    let maxid = users[0].id;
-    for (let i = 1; i < users.length; i++) {
-        if (users[i].id > maxid) maxid = users[i].id;
-        // console.log(users[i].id);
-    }
-    return maxid + 1;
-}
+
 app.get('/', (req, res) => {
     res.send(users);
 });
@@ -25,14 +21,13 @@ app.get('/', (req, res) => {
 app.get('/users/:id', (req, res) => {
     for (const user of users) {
         if (user.id === +req.params.id)
-    // const id = req.params.id;
     res.send(user);
 }});
 
 app.post('/newUsers', jsonParser, (req, res) => {
-    const newUser = { id: maxid(users), email: req.body.email, password: req.body.password };
+    const newUser = { id: uuidv4(), email: req.body.email, password: req.body.password };
     users.push(newUser);
-    // console.log(users);
+    console.log(users);
     res.send("hay");
 });
 
