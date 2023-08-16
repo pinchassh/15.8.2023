5b4b014515d9186a11d80f04dc3884bdd6bda3a6
pinchassh <psh28497@gmail.com>
2023-08-16 17:17:45 +0300

    midle part D

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const { log } = require('console');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
-const uuid = uuidv4()
-// console.log(uuid);
 const app = express();
 const port = 3000;
 const jsonParser = express.json();
+const bcrypt = require('bcryptjs');
+
 
 const users = [
-    { id: uuidv4(), email: 'avi@example.com', password: 'password123' },
+    { id: uuidv4(), email: 'avi@example.com', password: '$2a$10$JFTyCRzgXhbnE2PmPpLD8.bJP8HIGFWeGao3qGZD2jyHLWb0gQyc.' },
     { id: uuidv4(), email: 'dana@example.com', password: 'securepass' },
     { id: uuidv4(), email: 'yael@example.com', password: 'mysecretpw' }
 ];
 
 
+
+
 app.get('/', (req, res) => {
     res.send(users);
 });
+
 
 app.get('/users/:id', (req, res) => {
     for (const user of users) {
@@ -25,12 +28,18 @@ app.get('/users/:id', (req, res) => {
     }
 });
 
+
 app.post('/newUsers', jsonParser, (req, res) => {
-    const newUser = { id: uuidv4(), email: req.body.email, password: req.body.password };
-    users.push(newUser);
-    console.log(users);
-    res.send("hay");
-});
+    bcrypt.hash(req.body.password, 10).then((pass) => {
+        const newUser = { id: uuidv4(), email: req.body.email, password: pass };
+        users.push(newUser);
+        console.log(users);
+        res.send("hay");
+    });
+})
+
+
+
 
 app.put('/putUsers/:id', jsonParser, (req, res) => {
     for (const user of users) {
@@ -55,12 +64,30 @@ app.delete('/deleteUser/:id', jsonParser, (req, res) => {
 });
 
 
-app.post('/chackUser', jsonParser, (req, res) => {
+app.post('/checkUser', jsonParser, (req, res) => {
+    // let fleg = false;
     for (const user of users) {
-        if (user.email === req.body.email && user.password === req.body.password){return res.send('User is connected!')}
+        // bcrypt.compare(user.password, req.body.password).then((a)=>console.log(a))
+        
+        if (user.email === req.body.email) {
+             bcrypt.compare(user.password, req.body.password).then((check)=>{
+                console.log(check);
+                if(check){res.send('User is connected!');
+                // fleg = true;
+                return}
+             })
+            
+        }
     }
-    return res.send('wrong credentials!')
+    res.send('wrong credentials!')
 });
+// bcrypt.hash(users[0].password, 10)
+//     .then((v) => {
+//         console.log(v);
+//         // return bcrypt.compare(v, a)
+//     })
+
+
 
 app.listen(port, () => {
     console.log(`Server is up and running on port:${port}`);
